8e8a73c22b338e32c79635ba11b9d290a1a709fb
MelWang416 <1743207520@qq.com>
2025-10-02 23:29:37 -0700

    first commit

--- hardware/wk1/hw1/verilog/mac.v
+++ hardware/wk1/hw1/verilog/mac.v
@@ -16,12 +16,95 @@ input signed [bw-1:0] B;
 output signed [psum_bw-1:0] out;
 
 reg signed [psum_bw-1:0] psum_q;
+reg unsigned [psum_bw-1:0] psum_uq;
+reg signed [psum_bw-1:0] psum_mag;
+
 reg signed [bw-1:0] a_q;
 reg signed [bw-1:0] b_q;
+reg unsigned [bw-1:0] A_q;
+reg unsigned [bw-1:0] B_q;
+
 
 assign out = psum_q;
 
 // Your code goes here
 
+always@(posedge clk) begin
+	if(reset) begin
+		psum_q <= 15'b0;
+		psum_uq <= 15'b0;
+		a_q    <=  7'b0;
+		b_q    <=  7'b0;
+	end
+	else begin
+		a_q <= A;
+		b_q <= B;
+
+		if(acc) begin
+			if(format) begin
+		         	psum_q <= psum_uq;
+			end
+			else begin
+				psum_q <= psum_q + a_q*b_q;
+			end
+		end
+		else psum_q <= psum_q;
+	end
+end
+
+
+always@(*) begin
+	if(reset) begin
+		psum_uq = 15'b0;
+	end
+	else begin
+		if(acc) begin
+			if(format) begin
+		         	if(psum_uq[15] != psum_mag[15]) begin
+						if(psum_uq[psum_bw-2:0] > psum_mag[psum_bw-2:0]) begin
+							psum_uq[14:0] = psum_uq[14:0] - psum_mag[14:0];
+						end else begin
+                                                        psum_uq[14:0] = psum_mag[14:0] - psum_uq[14:0];	
+							psum_uq[15] = psum_mag[15];
+						end
+				end
+				else begin 
+					psum_uq[14:0] = psum_uq[14:0] + psum_mag[14:0]; 
+				end
+			end
+			else begin
+				psum_uq = 'b0;
+			end
+		end
+		else psum_uq = 'b0;
+	end
+end
+
+always@(*) begin
+	if(reset) begin
+	        psum_mag = 15'b0;
+		A_q = 7'b0;
+		B_q = 7'b0;
+	end
+	else begin
+		A_q = A;
+		B_q = B;
+		if(acc) begin
+			if(format) begin
+                                psum_mag = A_q[6:0]*B_q[6:0];
+				if(A_q[7] != B_q[7]) begin
+					psum_mag[15] = 1'b1;
+				end
+				else begin     
+					psum_mag[15] = 1'b0;
+				end
+			end
+			else psum_mag = 15'b0;
+		end
+		else psum_mag = 15'b0;;
+	end
+end
+					
+	      	
 
 endmodule
